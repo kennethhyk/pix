7db57756f77d80165e095414472f17d7e77e3948
kennethhyk <kennethhyk@gmail.com>
2018-12-08 22:05:08 -0800

    init

---
+++ Pix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pix
+//
+//  Created by Kenneth on 2018-12-08.
+//  Copyright © 2018 kennethOneManTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
